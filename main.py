4ec90e8900074f40b2bfae2c71219cc9813362f6
csd-oss <mk@buialo.com>
2020-07-21 18:57:28 +0300

    cleared main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sendMessage import send_message
-from time import sleep
 from userProfile import save_user
 
 
@@ -14,17 +13,9 @@ async def test(task, bot):
 
 
 async def main(task, bot):
-    print(task)
+    # print(task)
     if task['event'] == 'start':
         user = task['user']
         await save_user(user)
         task['text'] = 'Privet, напиши что-то'
         await send_message(task, bot)
-    else:
-        if task['message']['text'] == 'хуй':
-            task['text'] = 'Сук, у тебя хуй'
-            await send_message(task, bot)
-            sleep(3)
-            await question(task, bot)
-        else:
-            await question(task, bot)
